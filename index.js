1b33208a63f1be1f30ab7594706264bf19d12f28
Eric Nielsen <nielseneric2001@gmail.com>
2023-01-09 11:17:44 -0700

    Changed some wording.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     /* Progress bar display code (skills section) */
     const skillBar = document.querySelector("#skill-bar");
+    // Tags container element
+    const tagsContainer = document.querySelector(".skill-container");
+    // Tag elements contained within the tags container
     const tags = document.querySelectorAll(".tag");
     // Adding an event listener to each tag 
-    tags.forEach( (tag) => tag.addEventListener("mouseover", displayProgressBar) );
+    tags.forEach( (tag) => tag.addEventListener("mouseenter", displayProgressBar) );
     // Tag values, represented as objects
-    const tagObj = [  {"tName": "react", "value": 75, "color": "#61DBFB"}, 
-                      {"tName": "node", "value": 70, "color": "#68A063"}, 
-                      {"tName": "regex", "value": 65, "color": "orangered"},
-                      {"tName": "java", "value": 85, "color": "red"},
-                      {"tName": "bash", "value": 60, "color": "lightgreen"},
+    const tagObj = [  {"tName": "react", "value": 55, "color": "#61DBFB"}, 
+                      {"tName": "node", "value": 45, "color": "#68A063"}, 
+                      {"tName": "regex", "value": 35, "color": "orangered"},
+                      {"tName": "java", "value": 80, "color": "red"},
+                      {"tName": "sql", "value": 90, "color": "violet"},
                     ];
     function displayProgressBar() {
         const tagName = this.id // the ID of the tag that has been clicked
@@ -20,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function styleProgressBar(value, color){
         skillBar.value = value;
         skillBar.style.accentColor = color;
-        document.querySelector(".skill-container").style.display = "block";
+        tagsContainer.style.display = "block";
     }
     /* Work Showcase Divs Dynamic anchor - this removes the need forredundant anchor tags */
     const projectDivs = document.querySelectorAll(".project");
